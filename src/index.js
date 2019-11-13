8b368ab253b20c43d7a91eab7ad905740d1c364a
Morgan Galvin <morgan@ludwigthepig.com>
2019-11-12 21:28:30 -0700

    setup webpack and main constructor method

---
+++ src/index.js
@@ -0,0 +1,26 @@
+class GameController {
+  constructor(context, height) {
+    this.height = height;
+    this.width = (height / 9) * 16;
+    this.ctx = context;
+
+    this.init = this.init.bind(this);
+  }
+
+  init() {
+    this.ctx.height = this.height;
+    this.ctx.width = this.width;
+  }
+
+};
+
+window.onload = () => {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('webgl2')
+  const height = window.innerHeight * 0.98;
+  const gameController = new GameController(ctx, height);
+
+  gameController.init();
+}
+
+
