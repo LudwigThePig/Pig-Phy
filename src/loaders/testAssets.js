e615e23636380e3e349fad709297b38a4f30387d
Morgan Galvin <morgan@ludwigthepig.com>
2019-11-26 20:29:06 -0700

    feat: add shadows to spheres and cubes
    No breaking changes

--- src/loaders/testAssets.js
+++ src/loaders/testAssets.js
@@ -11,17 +11,21 @@ const defaultPos = () => ({
 export class Cube {
   constructor(height = 1, width = 1, depth = 1, pos = defaultPos()) {
     this.geometry = new THREE.BoxGeometry(height, width, depth);
-    this.material = new THREE.MeshBasicMaterial({ color: colors.pink });
+    this.material = new THREE.MeshPhongMaterial({ color: colors.pink });
     this.cube = new THREE.Mesh(this.geometry, this.material);
     this.cube.position.set(pos.x, height / 2, pos.z);
+    this.cube.castShadow = true;
+    this.cube.receiveShadow = true;
   }
 }
 
 export class Sphere {
   constructor(radius = 1, pos = defaultPos()) {
-    this.geometry = new THREE.SphereGeometry(radius, 32, 32);
-    this.material = new THREE.MeshBasicMaterial({ color: colors.purple });
+    this.geometry = new THREE.SphereGeometry(radius, 12, 12);
+    this.material = new THREE.MeshPhongMaterial({ color: colors.purple });
     this.sphere = new THREE.Mesh(this.geometry, this.material);
     this.sphere.position.set(pos.x, radius, pos.z);
+    this.sphere.castShadow = true;
+    this.sphere.receiveShadow = true;
   }
 }
