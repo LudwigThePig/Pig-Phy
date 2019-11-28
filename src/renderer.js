5e3a61acf78566f247862538840208d95e2a7592
Morgan Galvin <morgan@ludwigthepig.com>
2019-11-27 19:23:16 -0700

    feat: setup collision detection structure
    No working implementation, just the worlflow/etc
    
    No breaking changes

--- src/renderer.js
+++ src/renderer.js
@@ -7,10 +7,10 @@ import { Cube, Sphere } from './loaders/testAssets';
 
 const OrbitControls = require('three-orbit-controls')(THREE);
 
-/* ******
-* Input State *
-******* */
 
+/* ************
+* Input State *
+************* */
 const keyboard = {};
 const getKeyCode = event => event.which;
 export const keydown = event => { keyboard[getKeyCode(event)] = true; };
@@ -37,7 +37,6 @@ scene.background = new THREE.Color(color.black);
 /* *******
 * Camera *
 ******** */
-// const camera = new THREE.PerspectiveCamera(70, width / height, 0.1, 1000);
 const camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
 camera.position.set(0, 1, -3);
 camera.lookAt(scene.position);
@@ -62,6 +61,16 @@ topLight.shadowDarkness = 2;
 
 topLight.shadowCameraVisible = true; // for debugging
 scene.add(topLight);
+
+
+/* *************
+* Rigid Bodies *
+************** */
+const rigidBodies = [];
+const applyRigidBody = mesh => {
+  if (Array.isArray(mesh)) rigidBodies.push(...mesh);
+  else rigidBodies.push(mesh);
+};
 
 
 /* ********
@@ -106,12 +115,14 @@ const cubes = Array(20)
     scene.add(cube);
     return cube;
   });
+applyRigidBody(cubes);
 
 const spheres = Array(20).fill(0).map(() => {
   const { sphere } = new Sphere();
   scene.add(sphere);
   return sphere;
 });
+applyRigidBody(spheres);
 
 
 /* ********
@@ -124,8 +135,34 @@ loader.load( // pig
   err => console.error(err),
 );
 
+
+// COLLISION DETECTION
+const checkCollision = () => {
+  rigidBodies.forEach(bodies => {
+    bodies.material.transparent = false;
+    bodies.material.opacity = 1.0;
+  });
+
+  const cube = scene.getObjectByName('cube');
+  const originPoint = cube.position.clone();
+  for (let vertexIndex = 0; vertexIndex < cube.geometry.vertices.length; vertexIndex++) {
+    const localVertex = cube.geometry.vertices[vertexIndex].clone();
+    const globalVertex = localVertex.applyMatrix4(cube.matrix);
+    const directionVector = globalVertex.sub(cube.position);
+    const ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
+    const collisionResults = ray.intersectObjects(rigidBodies);
+    if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length()) {
+      console.log(collisionResults[0].object.name);
+      collisionResults[0].object.material.transparent = true;
+      collisionResults[0].object.material.opacity = 0.4;
+    }
+  }
+};
+
+
 // MAIN FUNC
 const draw = () => {
+  checkCollision();
   controls.update();
   requestAnimationFrame(draw);
 
