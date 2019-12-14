f715ebc64f85f0317bb89d4a50a4392774cfdc65
Morgan Galvin <morgan@ludwigthepig.com>
2019-12-14 11:17:51 -0800

    inv: pseudocode out a verlet velocity integration
    The prior method is known as the leapfrog method, which can produce
    inconsitent results if the physics engine is running at a different
    rate.

--- src/physics/Force.js
+++ src/physics/Force.js
@@ -1,7 +1,32 @@
 export default class Force {
-  static forceAndMassToPos(force, mass, curVelocity, timeStep, position) {
+  /**
+   * A straightforward way to calculate the new pos
+   * of an object, given a force
+   */
+  static leapFrogPos(force, mass, curVelocity, timeStep, curPos) {
     const acceleration = force / mass;
     const velocity = curVelocity + (acceleration * timeStep);
-    return position + (velocity * timeStep);
+    const position = curPos + (velocity * timeStep);
+    return { velocity, position };
+  }
+
+  /**
+   * A more refined approach than the leapfrog approach
+   * https://en.wikipedia.org/wiki/Verlet_integration#Velocity_Verlet
+   */
+  static verletPos(
+    acceleration,
+    curVelocity,
+    force,
+    mass,
+    timeStep,
+    curPos,
+  ) {
+    const lastAcceleration = acceleration;
+    const position = curPos + curVelocity * timeStep + (0.5 * lastAcceleration * timeStep ^ 2);
+    const newAcceleration = force / mass;
+    const avgAcceleration = (lastAcceleration + newAcceleration) / 2;
+    const velocity = curVelocity + avgAcceleration * timeStep;
+    return { velocity, position };
   }
 }
