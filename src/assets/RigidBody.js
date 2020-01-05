5213b8452c4963ca0226a140118a0a21262b2a5a
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-04 21:25:33 -0700

    feat: created RigidBody base class that will apply to all RigidBodies

---
+++ src/assets/RigidBody.js
@@ -0,0 +1,19 @@
+export default class RigidBody {
+  constructor() {
+    this.f = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    this.v = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    this.a = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+  }
+}
