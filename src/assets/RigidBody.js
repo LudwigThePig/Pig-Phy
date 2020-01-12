c7803a662b7f98cd6ff6bc0c23af423854a30e9b
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-12 16:57:54 -0700

    rfc: move all physics stuffz in to a physics obj

--- src/assets/RigidBody.js
+++ src/assets/RigidBody.js
@@ -3,26 +3,30 @@ export default class RigidBody {
    * @param {number} mass mass of rigid body in kg
    */
   constructor(mesh, mass = 10) {
-    this.d = { // Displacement
-      x: 0,
-      y: 0,
-      z: 0,
+    this.physics = {
+
+      d: { // Displacement
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      f: { // Force
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      v: { // Velocity
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      // Acceleration
+      a: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      mass,
     };
-    this.f = { // Force
-      x: 0,
-      y: 0,
-      z: 0,
-    };
-    this.v = { // Velocity
-      x: 0,
-      y: 0,
-      z: 0,
-    };
-    this.a = { // Acceleration
-      x: 0,
-      y: 0,
-      z: 0,
-    };
-    this.mass = mass;
   }
 }
