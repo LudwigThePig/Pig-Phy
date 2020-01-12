47d428453083aa3d01e936bbf4959198a85bb320
Morgan Galvin <morgan@ludwigthepig.com>
2020-01-12 16:11:49 -0700

    rfc: remove the abstracted complex stuff for specific/hard coded stuff

--- src/gameManager.js
+++ src/gameManager.js
@@ -1,37 +1,40 @@
-/* eslint-disable max-classes-per-file */
-// * ___________Components___________
-const moveables = () => {
-
-};
-
-const renderables = (x, y, z) => {
-
-};
-
-// * ___________Systems___________
-
-
 export default class GameManager {
   constructor() {
-    this.index = 0;
+    // These are the components
+    this.meshes = [];
+    this.rbs = [];
+
+    // Entity Stuff
+    this.pointer = 0;
     this.entities = [];
+
+    //*  Misc Settings
+    // Time
+    this.d1 = new Date().valueOf(); // Current timestamp
+    this.d2 = new Date().valueOf(); // Timestamp of last frame
+    this.dt = 0.00; // delta time between frames
+
+    // Forces
+    this.jumpForce = 500;
+    this.gravityForce = -9.81; // meters per second
+
+
+    // Force Coefficients
+    this.e = -0.2; // Coefficient of restitution ('bounciness' of the floor)
+    this.rho = 1.2; // Density of air. Try 1000 for water.
+    this.coefficientAir = 0.47; // Air Resistance
+    this.coefficientGround = 0.1; // Ground Friction
   }
 
   createEntity() {
-    this.index++;
-    this.entities[this.index] = undefined;
-    return this.index;
-  }
-}
-
-
-export class Entity {
-  constructor(gameManager, pointer) {
-    this.gameManager = gameManager;
-    this.pointer = pointer;
+    this.pointer++;
+    this.entities[this.pointer] = undefined;
+    return this.pointer;
   }
 
-  addComponent(component, value) {
-
+  updateDeltaTime() {
+    this.d2 = this.d1;
+    this.d1 = new Date().valueOf();
+    this.dt = (this.d1 - this.d2) / 1000;
   }
 }
